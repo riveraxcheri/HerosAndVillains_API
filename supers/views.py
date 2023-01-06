abab9255feebd3d1d4707722c3d4f218679b7a31
riveraxcheri <117052183+riveraxcheri@users.noreply.github.com>
2023-01-06 02:35:54 -0600

    Created 'PUT' funtion in the Supers Views file

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -16,11 +17,14 @@ def supers_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 def supers_detail(request, pk):
-    
-    try:
-        supers = Super.objects.get(pk=pk)
-        serializer = SuperSerializer(supers);
-    except Super.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND);
+    supers = get_object_or_404(Super, pk=pk)
+    if request.method == 'GET':
+        serializer = SuperSerializer(supers);        
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SuperSerializer(supers, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
